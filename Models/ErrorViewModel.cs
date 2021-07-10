429842cc599b28d86487d40579e0f55b9b93e2bc
Vaibhav <vaibhav.maurya@centricconsulting.com>
2021-07-10 23:33:19 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DummyGitTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
